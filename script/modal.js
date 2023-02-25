a9dbd5aef579e0b208baacf5c19b213f22c96d50
smky_OdenD_SP4 <tmd_bkcoffee@outlook.jp>
2023-02-25 23:36:18 +0900

    Adjusted padding on "wrap" item.

--- script/modal.js
+++ script/modal.js
@@ -21,3 +21,11 @@ $(".video-open").modaal({
       $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
     }
 });
+
+// ページ内遷移
+$('#g-navi a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
